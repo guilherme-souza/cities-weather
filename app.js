16cdce947ce0733b72f6cae3a886e30e3a25aa1b
Guilherme <e049577@mastercard.com>
2017-06-17 11:16:22 -0300

    Adding panel with more information about the weather of a selected city

--- app.js
+++ app.js
@@ -11,31 +11,43 @@ var cityWeatherController = function($scope, cityWeatherService) {
     {
       name: 'Amsterdam',
       image: 'amsterdam.png',
-      url: 'http://api.openweathermap.org/data/2.5/weather?q=Amsterdam,nl&appid=3d8b309701a13f65b660fa2c64cdc517&units=metric'
+      url: 'http://api.openweathermap.org/data/2.5/weather?q=Amsterdam,nl&appid=3d8b309701a13f65b660fa2c64cdc517&units=metric',
+      urlForecast: 'http://api.openweathermap.org/data/2.5/forecast?q=Amsterdam,nl&appid=3d8b309701a13f65b660fa2c64cdc517&units=metric'
     }, {
       name: 'Lisbon',
       image: 'lisbon.png',
-      url: 'http://api.openweathermap.org/data/2.5/weather?q=Lisbon,pt&appid=3d8b309701a13f65b660fa2c64cdc517&units=metric'
+      url: 'http://api.openweathermap.org/data/2.5/weather?q=Lisbon,pt&appid=3d8b309701a13f65b660fa2c64cdc517&units=metric',
+      urlForecast: 'http://api.openweathermap.org/data/2.5/forecast?q=Lisbon,pt&appid=3d8b309701a13f65b660fa2c64cdc517&units=metric'
     }, {
       name: 'London',
       image: 'london.png',
-      url: 'http://api.openweathermap.org/data/2.5/weather?q=London,uk&appid=3d8b309701a13f65b660fa2c64cdc517&units=metric'
+      url: 'http://api.openweathermap.org/data/2.5/weather?q=London,uk&appid=3d8b309701a13f65b660fa2c64cdc517&units=metric',
+      urlForecast: 'http://api.openweathermap.org/data/2.5/forecast?q=London,uk&appid=3d8b309701a13f65b660fa2c64cdc517&units=metric'
     }, {
       name: 'Madrid',
       image: 'madrid.png',
-      url: 'http://api.openweathermap.org/data/2.5/weather?q=Madrid,es&appid=3d8b309701a13f65b660fa2c64cdc517&units=metric'
+      url: 'http://api.openweathermap.org/data/2.5/weather?q=Madrid,es&appid=3d8b309701a13f65b660fa2c64cdc517&units=metric',
+      urlForecast: 'http://api.openweathermap.org/data/2.5/forecast?q=Madrid,es&appid=3d8b309701a13f65b660fa2c64cdc517&units=metric'
     }, {
       name: 'Paris',
       image: 'paris.png',
-      url: 'http://api.openweathermap.org/data/2.5/weather?q=Paris,fr&appid=3d8b309701a13f65b660fa2c64cdc517&units=metric'
+      url: 'http://api.openweathermap.org/data/2.5/weather?q=Paris,fr&appid=3d8b309701a13f65b660fa2c64cdc517&units=metric',
+      urlForecast: 'http://api.openweathermap.org/data/2.5/forecast?q=Paris,fr&appid=3d8b309701a13f65b660fa2c64cdc517&units=metric'
     }
   ];
+
+  $scope.selectedCity = '';
 
   cityWeatherService.getCityWeather($scope.cities[0]);
   cityWeatherService.getCityWeather($scope.cities[1]);
   cityWeatherService.getCityWeather($scope.cities[2]);
   cityWeatherService.getCityWeather($scope.cities[3]);
   cityWeatherService.getCityWeather($scope.cities[4]);
+
+  $scope.selectCity = function(city) {
+    cityWeatherService.getCityForecast(city);
+    $scope.selectedCity = city;
+  };
 }
 
 cityWeatherController.$inject = ['$scope', 'CityWeatherService'];
@@ -51,6 +63,26 @@ var cityWeatherService = function($http) {
         cityObject.temperature = resp.main.temp;
         cityObject.weatherDescription = resp.weather[0].description;
         cityObject.windSpeed = resp.wind.speed;
+      });
+    },
+    getCityForecast: function(cityObject) {
+      $http.get(cityObject.urlForecast).then(function(response) {
+        var resp = response.data;
+        cityObject.threeHours = {
+          temperature: resp.list[0].main.temp,
+          weather: resp.list[0].weather[0].description,
+          wind: resp.list[0].wind.speed
+        };
+        cityObject.sixHours = {
+          temperature: resp.list[1].main.temp,
+          weather: resp.list[1].weather[0].description,
+          wind: resp.list[1].wind.speed
+        };
+        cityObject.nineHours = {
+          temperature: resp.list[2].main.temp,
+          weather: resp.list[2].weather[0].description,
+          wind: resp.list[2].wind.speed
+        };
       });
     }
   };
